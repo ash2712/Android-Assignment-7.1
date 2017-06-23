e12d7ea317ddc027d91a9c86a7a06cad97c94cbc
ash2712 <spellguru@gmail.com>
2017-06-23 21:44:13 +0530

    Add files via upload

---
+++ src/main/java/com/example/ayush/googlesearchapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ayush.googlesearchapplication;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText et1 = (EditText) findViewById(R.id.editext);
+        Button bt1 = (Button) findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                String term = et1.getText().toString();
+                intent.putExtra(SearchManager.QUERY, term);
+                startActivity(intent);
+            }
+        });
+    }
+}
